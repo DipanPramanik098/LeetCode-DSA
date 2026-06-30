3884a2b8a28cf41b99088f874cd9a5323ab98904
Dipan Pramanik <127842320+DipanPramanik098@users.noreply.github.com>
2026-06-30 06:48:10 +0530

    Time: 5 ms (82.45%), Space: 46.7 MB (26.38%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int maxLen = 0;
+
+        // Push sentinel value
+        st.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    int len = i - st.peek();
+                    maxLen = Math.max(maxLen, len);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
