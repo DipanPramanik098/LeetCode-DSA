381e690a2da9543c7b0aa1a96cf7ddc7c11dc78a
Dipan Pramanik <127842320+DipanPramanik098@users.noreply.github.com>
2026-07-28 06:35:16 +0530

    Time: 1 ms (68.36%), Space: 42.9 MB (29.52%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+
+        // Base Case
+        dp[0] = 1;
+
+        for (int t = 1; t <= target; t++) {
+
+            for (int num : nums) {
+
+                if (t >= num)
+                    dp[t] += dp[t - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
