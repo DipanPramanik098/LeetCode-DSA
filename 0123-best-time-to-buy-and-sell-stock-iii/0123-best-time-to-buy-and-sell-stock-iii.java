e1b6f3f35fd2d4c3c0738a29249c415940c9ea31
Dipan Pramanik <127842320+DipanPramanik098@users.noreply.github.com>
2026-08-05 10:49:27 +0530

    Time: 57 ms (36.34%), Space: 117.8 MB (33.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -40,19 +40,53 @@ class Solution {
             );
         }
     }
+
+    public static int bottomUp(int [] prices){
+        int  n = prices.length, no_of_transaction = 2, buy = 1;
+
+        int [] [] [] dp = new int[n+1][2][3];
+
+        for(int td [][] : dp){
+            for(int row[] : td){
+                Arrays.fill(row, 0); // to handle base -> k =0 sab 0, i=n sab 0
+            }
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=buy; j++){
+                for(int k=1; k<= no_of_transaction; k++){  // k 1 se start hai karan no of transaction 0 is base case
+                    if(j==1){
+                        dp[i][j][k] = Math.max(
+                            -prices[i] + dp[i+1][0][k],
+                            dp[i+1][1][k]
+                        );
+                    }else{
+                        dp[i][j][k] = Math.max(
+                            prices[i] + dp[i+1][1][k-1],
+                            dp[i+1][0][k]
+                        );
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
     public int maxProfit(int[] prices) {
         int i = 0, n = prices.length, no_of_transaction = 2, buy = 1;
         //Recursive Solution
         // return maxProfit(i, n, buy, no_of_transaction, prices);
 
         // Top Down DP
-        int [] [] [] dp = new int[n+1][2][3];
+        // int [] [] [] dp = new int[n+1][2][3];
 
-        for(int td [][] : dp){
-            for(int row[] : td){
-                Arrays.fill(row, -1);
-            }
-        }
-        return maxProfit1(i,n,buy, no_of_transaction, prices, dp);
+        // for(int td [][] : dp){
+        //     for(int row[] : td){
+        //         Arrays.fill(row, -1);
+        //     }
+        // }
+        // return maxProfit1(i,n,buy, no_of_transaction, prices, dp);
+
+        // Bottom Up
+        return bottomUp(prices);
     }
 }
