af7f58d8b68fa1522a83cac36318250afe831aa7
Dipan Pramanik <127842320+DipanPramanik098@users.noreply.github.com>
2026-08-26 20:24:29 +0530

    Time: 3 ms (4.87%), Space: 42.2 MB (34.08%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        Boolean[] dp = new Boolean[n + 1];
+        return solve(n, dp);
+    }
+
+    private boolean solve(int n, Boolean[] dp) {
+        if (n == 1) {
+            return false;
+        }
+
+        if (dp[n] != null) {
+            return dp[n];
+        }
+
+        for (int x = 1; x < n; x++) {
+            if (n % x == 0) {
+                if (!solve(n - x, dp)) {
+                    return dp[n] = true;
+                }
+            }
+        }
+
+        return dp[n] = false;
+    }
+}
